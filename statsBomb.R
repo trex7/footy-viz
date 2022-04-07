c0897eabf626a15c3db6a6cffa74c6f0dc1adf12
timschott <tcs9pk@virginia.edu>
2022-04-07 14:52:22 -0700

    heatmap code

--- statsBomb.R
+++ statsBomb.R
@@ -7,7 +7,6 @@ library(devtools)
 ## remotes::install_version("SDMTools", "1.1-221")
 ## devtools::install_github("statsbomb/StatsBombR")
 library(StatsBombR)
-
 ## load free data
 all_comps <- FreeCompetitions()
 
@@ -105,3 +104,104 @@ hk_shots <- england_event_data %>%
 colnames(hk_shots)
 
 create_Shot_Map(hk_shots, "Harry Kane Shot Map", "WC 2018 + Euro 2020", xG = TRUE)    
+
+## heatmap -- where is HK playing? -- offensive actions only...
+offensive_events <- c("Ball Receipt*", "Ball Recovery","Carry",
+                      "Dispossessed","Dribble","Foul",
+                      "Injury","Stoppage","Miscontrol","Pass","Shot")
+
+hk_offensive <- full_hk %>% 
+  filter(type.name %in% offensive_events)
+
+heatmap <- hk_offensive %>% mutate(location.x = ifelse(location.x>120, 120, location.x),
+            location.y = ifelse(location.y>80, 80, location.y),
+            location.x = ifelse(location.x<0, 0, location.x),
+            location.y = ifelse(location.y<0, 0, location.y))
+
+# 6 x bins
+heatmap$xbin <- cut(heatmap$location.x, breaks = seq(from=0, to=120, by = 20),include.lowest=TRUE )
+# 4 y bins
+heatmap$ybin <- cut(heatmap$location.y, breaks = seq(from=0, to=80, by = 20),include.lowest=TRUE)
+
+binned_heatmap <- heatmap %>%
+  mutate(total_OA = n()) %>%
+  group_by(xbin, ybin) %>%
+  summarise(bin_OA = n(),
+            bin_pct = bin_OA/total_OA,
+            location.x = median(location.x),
+            location.y = median(location.y)) %>%
+  group_by(xbin, ybin)
+
+# need to add a few more so its fully filled out.
+distinct_binned_heatmap <- distinct(binned_heatmap)
+
+temp_bins <- read.csv("temp-bins.csv", header = FALSE)
+colnames(temp_bins) <- colnames(distinct_binned_heatmap)
+
+# recode xbin
+temp_bins$xbin <- as.factor(temp_bins$xbin)
+# re do its levels
+levels(temp_bins$xbin) <- levels(distinct_binned_heatmap$xbin)
+
+temp_bins$xbin[1] <- distinct_binned_heatmap$xbin[1]
+
+# same for ybin
+temp_bins$ybin <- as.factor(temp_bins$ybin)
+# re do its levels
+levels(temp_bins$ybin) <- levels(distinct_binned_heatmap$ybin)
+
+# now combine them
+full_binned_heatmap <- as.data.frame(rbind(distinct_binned_heatmap, temp_bins))
+
+activitycolors <- c("#dc2429", "#dc2329", "#df272d", "#df3238", "#e14348", "#e44d51",
+                             "#e35256", "#e76266", "#e9777b", "#ec8589", "#ec898d", "#ef9195",
+                             "#ef9ea1", "#f0a6a9", "#f2abae", "#f4b9bc", "#f8d1d2", "#f9e0e2",
+                             "#f7e1e3", "#f5e2e4", "#d4d5d8", "#d1d3d8", "#cdd2d6", "#c8cdd3", "#c0c7cd",
+                             "#b9c0c8", "#b5bcc3", "#909ba5", "#8f9aa5", "#818c98", "#798590",
+                             "#697785", "#526173", "#435367", "#3a4b60", "#2e4257", "#1d3048",
+                             "#11263e", "#11273e", "#0d233a", "#020c16")
+
+ggplot(data = distinct_binned_heatmap, aes(x = location.x, y = location.y, fill = bin_pct, group=bin_pct)) +
+  geom_bin2d(binwidth = c(20, 20), position = "identity", alpha = 0.9) + #2
+  annotate("rect",xmin = 0, xmax = 120, ymin = 0, ymax = 80, fill = NA, colour = "black", size = 0.6) +
+  annotate("rect",xmin = 0, xmax = 60, ymin = 0, ymax = 80, fill = NA, colour = "black", size = 0.6) +
+  annotate("rect",xmin = 18, xmax = 0, ymin = 18, ymax = 62, fill = NA, colour = "white", size = 0.6) +
+  annotate("rect",xmin = 102, xmax = 120, ymin = 18, ymax = 62, fill = NA, colour = "white", size = 0.6) +
+  annotate("rect",xmin = 0, xmax = 6, ymin = 30, ymax = 50, fill = NA, colour = "white", size = 0.6) +
+  annotate("rect",xmin = 120, xmax = 114, ymin = 30, ymax = 50, fill = NA, colour = "white", size = 0.6) +
+  annotate("rect",xmin = 120, xmax = 120.5, ymin =36, ymax = 44, fill = NA, colour = "black", size = 0.6) +
+  annotate("rect",xmin = 0, xmax = -0.5, ymin =36, ymax = 44, fill = NA, colour = "black", size = 0.6) +
+  annotate("segment", x = 60, xend = 60, y = -0.5, yend = 80.5, colour = "white", size = 0.6)+
+  annotate("segment", x = 0, xend = 0, y = 0, yend = 80, colour = "black", size = 0.6)+
+  annotate("segment", x = 120, xend = 120, y = 0, yend = 80, colour = "black", size = 0.6)+
+  theme(rect = element_blank(),
+        line = element_blank()) +
+  annotate("point", x = 12 , y = 40, colour = "white", size = 1.05) +
+  annotate("point", x = 108 , y = 40, colour = "white", size = 1.05) +
+  annotate("path", colour = "white", size = 0.6,
+           x=60+10*cos(seq(0,2*pi,length.out=2000)),
+           y=40+10*sin(seq(0,2*pi,length.out=2000)))+
+  annotate("point", x = 60 , y = 40, colour = "white", size = 1.05) +
+  annotate("path", x=12+10*cos(seq(-0.3*pi,0.3*pi,length.out=30)), size = 0.6,
+           y=40+10*sin(seq(-0.3*pi,0.3*pi,length.out=30)), col="white") +
+  annotate("path", x=108-10*cos(seq(-0.3*pi,0.3*pi,length.out=30)), size = 0.6,
+           y=40-10*sin(seq(-0.3*pi,0.3*pi,length.out=30)), col="white") +
+  theme(axis.text.x=element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        plot.caption=element_text(size=13, hjust=0.5, vjust=0.5),
+        plot.subtitle = element_text(size = 15, hjust = 0.5),
+        axis.text.y=element_blank(),
+        legend.title = element_blank(),
+        legend.text=element_text(size=10),
+        legend.key.size = unit(1, "cm"),
+        plot.title = element_text(margin = margin(r = 10, b = 10), face="bold",size = 20, colour = "black", hjust = 0.5),
+        legend.direction = "vertical",
+        axis.ticks=element_blank(),
+        plot.background = element_rect(fill = "white"),
+        strip.text.x = element_text(size=13)) + #4
+  scale_y_reverse() + #5
+  scale_fill_gradientn(colours = activitycolors, na.value = "black", trans = "reverse", labels =
+                         scales::percent_format(accuracy = 1)) + #6
+  labs(title = "Where Does Harry Kane Attack on Average?", subtitle = "WC 2018 + Euro 2020") + #7
+  coord_fixed(ratio = 95/100)
